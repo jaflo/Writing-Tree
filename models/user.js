de535f506bf1746cdfcd5f6d3b1734d413760dc4
glooga <glooga@live.com>
2016-08-03 16:03:27 -0500

    more signup stuff, more work still needed (crashed at users above char limit[32] and general inefficiency

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ var bcrypt   = require('bcryptjs');
 var FLORIAN_STAT = 10;
 
 var userSchema = mongoose.Schema({
-	username: { type: String, required: true, index: { unique: true } },
-	email: { type: String, required: true, index: { unique: true } },
-	password: { type: String, required: true, index: { unique: true } },
+	username: { type: String, required: true, index: { unique: true }, maxlength: 32 },
+	email: { type: String, required: true, index: { unique: true }, maxlength: 500 },
+	password: { type: String, required: true, index: { unique: true }, minlength: 6 },
 	createdat: { type: Date, required: true, default: Date.now() },
 	changedat: { type: Date },
 	starred: [{ type: String }],
