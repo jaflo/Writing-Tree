28fd013dd7e70e16ab063664ec9a16e6098cedf4
glooga <glooga@live.com>
2016-08-03 19:56:40 -0500

    fixed the million jump endpoints that were there for some reason

--- app.js
+++ app.js
@@ -203,45 +203,6 @@ client.post('/placeholder-shortID/jump', function(req, res) { // not sure about 
 
 client.post('/placeholder-shortID/jump-same-author', function(req, res) { // same as above
     User.find({
-        parent: req.user.parent,
-        author: req.user.auther
-    }, function(err, stories) {
-        if (err) {
-            failRequest(req, res, "Error, try again later!");
-        }
-        if (stories.length == 1) {
-            failRequest(req, res, "No stories to jump to!");
-            return;
-        }
-        var story;
-        do {
-            story = stories[Math.floor(Math.random() * stories.length)];
-        } while (story.shortID == req.user.shortID);
-        completeRequest(req, res, story, "/story/" + story.shortID);
-    });
-});
-
-client.post('/placeholder-shortID/jump', function(req, res) { // not sure about url, should it be just "jump"?
-    User.find({
-        parent: req.body.parent
-    }, function(err, stories) {
-        if (err) {
-            failRequest(req, res, "Error, try again later!");
-        }
-        if (stories.length == 1) {
-            failRequest(req, res, "No stories to jump to!");
-            return;
-        }
-        var story;
-        do {
-            story = stories[Math.floor(Math.random() * stories.length)];
-        } while (story.shortID == req.body.shortID);
-        completeRequest(req, res, story, "/story/" + story.shortID);
-    });
-});
-
-client.post('/placeholder-shortID/jump-same-author', function(req, res) { // same as above
-    User.find({
         parent: req.body.parent,
         author: req.body.auther
     }, function(err, stories) {
@@ -260,26 +221,6 @@ client.post('/placeholder-shortID/jump-same-author', function(req, res) { // sam
     });
     master
 });
-
-client.post('/placeholder-shortID/jump', function(req, res) { // not sure about url, should it be just "jump"?
-    User.find({
-        parent: req.user.parent
-    }, function(err, stories) {
-        if (err) {
-            failRequest(req, res, "Error, try again later!");
-        }
-        if (stories.length == 1) {
-            failRequest(req, res, "No stories to jump to!");
-            return;
-        }
-        var story;
-        do {
-            story = stories[Math.floor(Math.random() * stories.length)];
-        } while (story.shortID == req.user.shortID);
-        completeRequest(req, res, story, "/story/" + story.shortID);
-    });
-});
-
 
 client.post('/star', function(req, res) {
     var temp_err = "";
